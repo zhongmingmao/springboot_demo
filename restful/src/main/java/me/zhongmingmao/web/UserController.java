996ab9894e0aeaf29ae4ae88eb330ce711fb450f
zhongmingmao <zhongmingmao0625@gmail.com>
2017-06-04 21:48:59 +0800

    Close #4 restful integrated with swagger2

--- restful/src/main/java/me/zhongmingmao/web/UserController.java
+++ restful/src/main/java/me/zhongmingmao/web/UserController.java
@@ -1,5 +1,8 @@
 package me.zhongmingmao.web;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import me.zhongmingmao.domain.User;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,22 +18,32 @@ public class UserController {
     private static ConcurrentHashMap<Long, User> users = new ConcurrentHashMap<>(); // 代替 DB
     private static String SUCCESS = "success";
     
+    @ApiOperation(value = "获取用户列表", notes = "")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<User> getAllUsers() {
         return new ArrayList<>(users.values());
     }
     
+    @ApiOperation(value = "创建用户", notes = "根据 User 对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户实体信息", required = true, dataType = "User")
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public String postUser(@ModelAttribute User user) {
         users.put(user.getId(), user);
         return SUCCESS;
     }
     
+    @ApiOperation(value = "获取用户详细信息", notes = "根据 id 获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户 ID", required = true, dataType = "Long")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public User getUser(@PathVariable Long id) {
         return users.get(id);
     }
     
+    @ApiOperation(value = "更新用户详细信息", notes = "根据 id 和传递过来的 user 信息更新用户详细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户 ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "user", value = "用户实体信息", required = true, dataType = "User")
+    })
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public String putUser(@PathVariable Long id, @ModelAttribute User user) {
         User u = users.get(id);
@@ -39,6 +52,9 @@ public class UserController {
         return SUCCESS;
     }
     
+    
+    @ApiOperation(value = "删除用户详细信息", notes = "根据 id 删除用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户 ID", required = true, dataType = "Long")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public String deleteUser(@PathVariable Long id) {
         users.remove(id);
