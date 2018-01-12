a6768d1ad06d6e714bea963e2f134b44fb4c23ac
zhongmingmao <zhongmingmao0625@gmail.com>
2018-01-12 23:04:22 +0800

    Multi Model

--- spring_boot_2/web/src/main/java/me/zhongmingmao/SpringBoot2Application.java
+++ spring_boot_2/web/src/main/java/me/zhongmingmao/SpringBoot2Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBoot2Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot2Application.class, args);
-	}
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2Application.class, args);
+    }
 }
